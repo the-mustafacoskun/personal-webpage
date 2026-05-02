1db3c1b058e16b9aa8d7a15411a2c1f1a739fb30
the-mustafacoskun <the.mustafacoskun@gmail.com>
2026-05-02 13:34:08 +0300

    Structure Created,installed packages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
